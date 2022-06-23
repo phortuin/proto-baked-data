6470905a1064304a72ea9dad997628defdfd0a7e
phortuin <phortuin@users.noreply.github.com>
2022-06-23 15:53:19 +0200

    feat: fancier pages

--- lib/page.ts
+++ lib/page.ts
@@ -30,21 +30,47 @@ function page(title:string, body:string):string {
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Baked Data!</title>
+    <script src="/bundle.js" defer></script>
     <style>
         body {
             font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,sans-serif;
+            background: #ffe;
+            line-height: 1.3;
         }
         main {
             margin-top: 2rem;
             padding: 1rem;
         }
+        main, nav, footer {
+            max-width: 40rem;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        nav ol {
+            list-style: none;
+            display: flex;
+            gap: 1rem;
+            margin: 0;
+            padding: 0;
+        }
         h1 {
-            font-size: 1.25em;
-            text-transform: uppercase;
-            letter-spacing: 2px;
+            font-size: 3em;
+            margin-left: -0.125em;
+            letter-spacing: 0.0625rem;
+            line-height: 1;
         }
         h1, p, pre {
             margin: 0 0 2rem;
+        }
+        h1, h2, h3, a {
+            color: #00f;
+        }
+        hr {
+            border: 0;
+            background: black;
+            height: 1px;
+            width: 100%;
+            margin: 2.5rem 0;
         }
         pre {
             font-family: 'Menlo', monospace;
@@ -54,9 +80,6 @@ function page(title:string, body:string):string {
             background: #f7f7f7;
             white-space: normal;
             font-size: 0.9375em;
-        }
-        a {
-            color: black;
         }
         .button {
             text-decoration: none;
@@ -75,11 +98,11 @@ function page(title:string, body:string):string {
             padding: 0;
         }
         .products li {
-            flex: 1 0 12vw;
-            height: 16rem;
+            flex: 1 0 6vw;
+            height: 12rem;
             margin: 0 0.5rem 1rem 0.5rem;
             padding: 1rem;
-            font-size: 2em;
+            font-size: 1.25em;
             border-radius: 0.5rem;
         }
         .products li a {
@@ -89,13 +112,32 @@ function page(title:string, body:string):string {
             width: 30vw;
             height: 20vh;
         }
+        input {
+            width: 6em;
+        }
+        input, button {
+            line-height: 1.3;
+        }
     </style>
 </head>
 <body>
+    <nav>
+        <ol>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li><a href="/blog">Blog</a></li>
+        </ol>
+    </nav>
     <main>
         <h1>${title}</h1>
         ${body}
     </main>
+    <footer>
+        <p>
+            Made with 🪠 in Bennebroek »
+            <a href="https://github.com/phortuin/proto-baked-data" target=_blank>Get it here &rarr;</a>
+        </p>
+    </footer>
 </body>
 </html>`
 }
